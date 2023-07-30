a22a4c5b92edbb5a1aaed4d5518b3e3eeab54c21
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-07-30 22:52:07 +0200

    layout and loading features

---
+++ src/app/layout.jsx
@@ -0,0 +1,23 @@
+import Navbar from "@/components/navbar/navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Next.js 13 Tut",
+  description: "Follow along project",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
