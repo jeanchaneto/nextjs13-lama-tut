c6a8968d2cf66eec17747248ec450103e3501ec6
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-07-31 15:24:16 +0200

    Home page - webkit gradient title with

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,21 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Hero from 'public/hero.png'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <div>
-      <Image src={Hero} alt="" className={styles.img}/>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>The react Framework practice</h1>
+        <p className={styles.description}>
+          Developping skills by learning Next.js 13. A super exiting project
+          covering the main features.
+        </p>
+        <button className={styles.button}>Learn more</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
     </div>
-  )
+  );
 }
