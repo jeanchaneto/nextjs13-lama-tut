25b233d785a164b8a4d3cc83c3e26b92b6af89f0
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-07-31 14:27:16 +0200

    Next.js Images

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import styles from './footer.module.css'
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>2023 WebDeveLap</div>
+      <div className={styles.social}>
+          <Image src="/1.png" width={16} height={16} className={styles.icon} alt="Social" />
+          <Image src="/2.png" width={16} height={16} className={styles.icon} alt="Social" />
+          <Image src="/3.png" width={16} height={16} className={styles.icon} alt="Social" />
+          <Image src="/4.png" width={16} height={16} className={styles.icon} alt="Social" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
