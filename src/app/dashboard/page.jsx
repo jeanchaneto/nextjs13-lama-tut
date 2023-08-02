2ac3b6c9d068b6ed8c8d1920a276bd2019c2d615
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-08-02 14:52:58 +0200

    Client side data fetching with use effect

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsloading] = useState(false);
 
-export default Dashboard
+  useEffect(() => {
+    const getData = async () => {
+      setIsloading(true);
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/`
+      );
+
+      if (!res.ok) {
+        setError(true);
+      }
+
+      setData(await res.json());
+      setIsloading(false);
+    };
+    getData();
+  }, []);
+console.log(data);
+
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
