d87a90d8d99a9c28a119716b40af8b4f5dc78d4d
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-08-02 14:35:35 +0200

    Server side data fetching

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image';
 
-const BlogPost = () => {
-  return (
-    <div className={styles["container"]}>BlogPost</div>
-  )
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
 }
 
-export default BlogPost
+const BlogPost = async ({params}) => {
+ 
+const data = await getData(params.id);
+
+  return (
+    // <div className={styles.container}>
+    //   <div className={styles.top}>
+    //     <div className={styles.info}>
+    //       <h1 className={styles.title}>{data.title}</h1>
+    //       <p className={styles.desc}>
+    //         {data.desc}
+    //       </p>
+    //       <div className={styles.author}>
+    //         <Image
+    //           src={data.img}
+    //           alt=""
+    //           width={40}
+    //           height={40}
+    //           className={styles.avatar}
+    //         />
+    //         <span className={styles.username}>{data.username}</span>
+    //       </div>
+    //     </div>
+    //     <div className={styles.imageContainer}>
+    //       <Image
+    //         src={data.img}
+    //         alt=""
+    //         fill={true}
+    //         className={styles.image}
+    //       />
+    //     </div>
+    //   </div>
+    //   <div className={styles.content}>
+    //     <p className={styles.text}>
+    //      {data.content}
+    //     </p>
+    //   </div>
+    // </div>
+    <h2>post</h2>
+  );
+};
+
+export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,47 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
+import styles from './page.module.css'
 
-const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
+
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
 }
+
+const Blog = async () => {
+
+  const data = await getData();
+
+  return (
+    <div className={styles.mainContainer}>
+      {data.map((item) => (
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={item.img}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default Blog
