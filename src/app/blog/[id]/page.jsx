2517cd08e48f292ce9e4212e9540404a515016ee
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-08-04 14:48:40 +0200

    Next url fix

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,44 +1,40 @@
-import React from 'react'
-import styles from './page.module.css'
-import Image from 'next/image';
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
-const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
-
 async function getData(id) {
+  const NEXT_URL =
+    process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
+
   const res = await fetch(`${NEXT_URL}/api/posts/${id}`, {
     cache: "no-store",
   });
- 
+
   if (!res.ok) {
     return notFound();
   }
- 
-  return res.json()
+
+  return res.json();
 }
 
-export async function generateMetadata(
-  { params }) {
-    const post = await getData(params.id)
-    return {
-      title: "NEXT.JS 13 tuto | Blog | "+post.title,
-      description: post.desc
-    }
-  }
+export async function generateMetadata({ params }) {
+  const post = await getData(params.id);
+  return {
+    title: "NEXT.JS 13 tuto | Blog | " + post.title,
+    description: post.desc,
+  };
+}
 
-const BlogPost = async ({params}) => {
-
-const data = await getData(params.id);
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
 
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>
-            {data.desc}
-          </p>
+          <p className={styles.desc}>{data.desc}</p>
           <div className={styles.author}>
             <Image
               src={data.img}
@@ -51,21 +47,13 @@ const data = await getData(params.id);
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image
-            src={data.img}
-            alt=""
-            fill={true}
-            className={styles.image}
-          />
+          <Image src={data.img} alt="" fill={true} className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>
-         {data.content}
-        </p>
+        <p className={styles.text}>{data.content}</p>
       </div>
     </div>
-  
   );
 };
 
